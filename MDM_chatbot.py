7eaf1ae06f6c8eac223a8d811e30aea318cd1f2d
getavik <53168086+getavik@users.noreply.github.com>
2019-08-28 15:12:14 +0530

    Update MDM_chatbot.py

--- MDM_chatbot.py
+++ MDM_chatbot.py
@@ -7,7 +7,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 import nltk
 import string
 import random
---import os
+#import os
 from tqdm import tqdm
 import requests
 
@@ -17,7 +17,7 @@ response = requests.get(url, stream=True)
 with open("10MB", "wb") as handle:
     for data in tqdm(response.iter_content()):
         handle.write(data)
---os.chdir('C:\\Users\\avikmukherjee\\Desktop\\My FIs\\Finance Master')
+#os.chdir('C:\\Users\\avikmukherjee\\Desktop\\My FIs\\Finance Master')
 f=open('Metric Definition List Text.txt','r',errors = 'ignore')
 raw=f.read()
 raw=raw.lower()# converts to lowercase
